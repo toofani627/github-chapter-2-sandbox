8af170ce80ac9df636bac58ee0a7c3bb3afc34e0
shashank3692580@gmail.com <shashank3692580@gmail.com>
2025-10-25 14:01:57 +0530

    hello

---
+++ main.js
@@ -0,0 +1,15 @@
+let name = "Alice"; // String
+const age = 30;    // Number
+let isStudent = true; // Boolean
+let hobbies = ["reading", "hiking", "coding"]; // Array
+let person = {  // Object
+    firstName: "John",
+    lastName: "Doe",
+    age: 25
+};
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies[0]); // Accessing array element
+console.log(person.firstName); // Accessing object property
